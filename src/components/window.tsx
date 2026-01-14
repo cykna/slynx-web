21cccb00440665c0e219d5efaa366101e87ab7c0
cykna <kina.lima1302@proton.me>
2026-01-14 11:26:56 -0300

    feat: rewritten window to react

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -1,24 +1,35 @@
+import {useState} from "react";
 import "../styles/globals.css";
 import "./window.css";
 import {PaintbrushVertical,ChevronRight,ChevronLeft} from "lucide-react";
 
-export default function Window({window_id, children, title}: {window_id: string, title:string, children: React.ReactNode}){ 
-    return <div id={window_id} class="slynx-window">  
-        <div class="window-title">
-            <div class="window-control-btns">
-                <div data-action="increase"></div>
-                <div></div>
-                <div data-action="decrease"></div>
-            </div>
-            <p class="title">{title}</p>
-            <div class="window-theme-btns">
-                <ChevronLeft class="prev" data-action="prev"/>
-                <PaintbrushVertical/>
-                <ChevronRight class="next" data-action="next"/>
-            </div>
-        </div>
-        <div class="window-content">
-            <slot/>
-        </div>
+interface WindowArgs {
+  window_id: string;
+  title: string;
+  children: React.ReactNode;
+  offset: number;
+  next: ()=>void;
+  prev: ()=>void;
+}
+
+export default function Window(args: WindowArgs){
+  const [width, s_width] = useState(100);
+  return <div id={args.window_id} className="slynx-window">
+    <div className="window-title">
+      <div className="window-control-btns">
+        <div data-action="increase"></div>
+        <div></div>
+        <div data-action="decrease"></div>
+      </div>
+      <p className="title">{args.title}</p> 
+      <div className="window-theme-btns">
+          <ChevronLeft classNameName="prev" onClick={args.prev} client:load/>
+          <PaintbrushVertical client:load/>
+          <ChevronRight classNameName="next" onClick={args.next} client:load/>
+      </div>
     </div>
+    <div className="window-content">
+      {args.children}
+    </div>
+  </div>
 }
