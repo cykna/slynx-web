91c23e7a841e40bfe66a4d3fcc4c7c6fe35de712
alanmachadozx <alanpinheirorj22@gmail.com>
2026-01-27 10:49:12 -0300

    feat: implement tags in roadmap

---
+++ src/components/roadmap.tsx
@@ -0,0 +1,72 @@
+import "./roadmap.css";
+
+export default function Roadmap() {
+    return (
+        <div className="struc">
+            <header>
+                <h1></h1>
+            </header>
+            <main>
+                <p>Legend</p>
+                <section>
+                    <p>Language Design</p>
+                    {/*arrow-right*/}
+                    <div>
+                        <ul>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <p>Front-end Implementation</p>
+                    {/*arrow-right*/}
+                    <div>
+                        <ul>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <p>Back-end & IR</p>
+                    {/*arrow-right*/}
+                    <div>
+                        <ul>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <p>Tooling</p>
+                    {/*arrow-right*/}
+                    <div>
+                        <ul>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                            <li>[]</li>
+                        </ul>
+                    </div>
+                </section>
+            </main>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from "react-router";
+
 import './index.css'
 import App from './App.tsx'
+import Roadmap from './components/roadmap.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/road" element={<Roadmap />} />
+    </Routes>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
