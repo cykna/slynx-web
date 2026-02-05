c6a7f5bd11d4d0a45726b86b70e298a58df06e22
cykna <kina.lima1302@proton.me>
2026-02-05 20:15:52 -0300

    feat: initialized page frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
 import { Footer } from "./components/footer/Footer";
 import Header from "./components/header/header";
 import PageFrame from "./components/pageFrame/PageFrame";
 import MainPage from "./pages/main/main";
-
 
 export default function Home() {
   return (
@@ -11,5 +9,4 @@ export default function Home() {
       <MainPage />
     </PageFrame>
   );
-
 }
