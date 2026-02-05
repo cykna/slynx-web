73f0bb99b2ed33c5de47efca2a2fd33b7ed5969b
cykna <kina.lima1302@proton.me>
2026-02-05 20:15:52 -0300

    feat: initialized page frame

---
+++ src/app/components/PageFrame.tsx
@@ -0,0 +1,19 @@
+import PageFrameStyles from "./PageFrame.module.css";
+
+export interface PageFrameProps {
+  children: React.ReactNode;
+  footer?: React.ReactNode;
+  header?: React.ReactNode;
+}
+
+export default function PageFrame(props: PageFrameProps) {
+  return (
+    <div className={PageFrameStyles.frame}>
+      <div className={PageFrameStyles.inner}>
+        {props.children}
+      </div>
+      {props.footer}
+    </div>
+
+  )
+}
