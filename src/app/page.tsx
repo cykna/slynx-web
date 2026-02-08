977c1710c31abe2d3598dc21de2f5987532cb049
cykna <kina.lima1302@proton.me>
2026-02-08 14:46:28 -0300

    feat: added support for header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import PageFrame from "./components/PageFrame";
+import PageFrame from "./components/PageFrame"
+
+function Header(){
+  return <div>Berimbau Bau Bau, berilum baubau</div>
+}
 
 export default function Home() {
   return (
