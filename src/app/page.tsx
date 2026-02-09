3d3bd08103bdc208c964523a7c7a2bd9e7e6a762
cykna <kina.lima1302@proton.me>
2026-02-09 12:18:53 -0300

    feat: implemented footer on the page frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,19 @@ import Header from "./components/header/header";
 import PageFrame from "./components/pageFrame/PageFrame";
 import MainPage from "./pages/main/main";
 
+function Footer() {
+  return <div>
+    <p>I thought i saw your face today</p>
+    <p>But then i turn my head away</p>
+    <p>And faced against the trees</p>
+    <p>But now it's just a memory</p>
+    <p>As they come... as they come</p>
+    <p>And i couldnt help</p>
+    <p>But fall in love again</p>
+    <p>No I couldnt help but fall in love again</p>
+  </div>
+}
+
 export default function Home() {
   return (
     <PageFrame header={<Header />} footer={<Footer />}>
