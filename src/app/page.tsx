306cdb99351b941d50becea810d3771a8a30c87c
cykna <kina.lima1302@proton.me>
2026-02-12 15:37:47 -0300

    feat: added footer of the frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,13 @@
 
-import styles from "./page.module.css";
-import PageFrame from "./components/PageFrame"
+import { Footer } from "./components/footer/Footer";
+import PageFrame from "./components/pageFrame/PageFrame";
 
-function Header(){
+function Header() {
   return <div>Berimbau Bau Bau, berilum baubau</div>
 }
-
-function Footer(){
-  return <div>
-    <p>I thought i saw your face today</p>
-    <p>But then i turn my head away</p>
-    <p>And faced against the trees</p>
-    <p>But now it's just a memory</p>
-    <p>As they come... as they come</p>
-    <p>And i couldnt help</p>
-    <p>But fall in love again</p>
-    <p>No I couldnt help but fall in love again</p>
-  </div>
-}
-
 export default function Home() {
   return (
-    <PageFrame header={<Header/>} footer={<Footer/>}>
+    <PageFrame header={<Header />} footer={<Footer />}>
       <p>Hello world</p>
       <p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p><p>Hello world</p>
       <p>Hello world</p>
