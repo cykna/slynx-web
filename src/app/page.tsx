50bbccc5bdfc2ddf228544f4fdf6ad21f1b1bb93
cykna <kina.lima1302@proton.me>
2026-02-18 09:10:23 -0300

    fix: fixed header to be placed inside frame

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 
 import { Footer } from "./components/footer/Footer";
+import Header from "./components/header/header";
 import PageFrame from "./components/pageFrame/PageFrame";
+import MainPage from "./pages/main/main";
 
-function Header() {
-  return <div>Berimbau Bau Bau, berilum baubau</div>
-}
+
 export default function Home() {
   return (
     <PageFrame header={<Header />} footer={<Footer />}>
